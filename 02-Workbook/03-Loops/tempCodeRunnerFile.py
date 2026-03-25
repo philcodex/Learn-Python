357d3b4370e83a828b4628de2e5e99fd1d4efb1a
philcodex <phil@icedcoffee.ie>
2026-03-25 16:25:26 +0000

    remove unrequired files

--- 02-Workbook/03-Loops/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-def common_letters():
-    str1 = input("Enter first string: ")
-    str2 = input("Enter second string: ")
-    s1 = set(str1)
-    s2 = set(str2)
-    print(s1)
-    print(s2)
-
-common_letters()
