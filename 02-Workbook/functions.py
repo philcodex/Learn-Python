eef81c001d1836db9ff7a511883d5cc5eaa2aae1
philokane <phil@icedcoffee.ie>
2026-07-18 18:50:49 +0100

    Add functions.py and loops.py

---
+++ 02-Workbook/functions.py
@@ -0,0 +1,10 @@
+# name = input('What is your name? ')
+# print(' Hi ' + name)
+
+
+name = input('What is your name? ')
+colour = input('What is your favourite colour? ')
+print(' Hi ' + name + ', your favourite colour is ' + colour)
+
+
+
---
+++ 02-Workbook/loops.py
@@ -0,0 +1,6 @@
+for i in range(5):
+    print("Checking server:", i)
+
+for i in range(5):
+    print(i)
+
